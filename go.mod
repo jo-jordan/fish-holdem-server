f85fd49b9259dad3b65a13dff4836d857dc80e9c
lzjlxebr <tyyzmyd@gmail.com>
2022-11-09 19:54:15 +0800

    init commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jo-jordan/fish-holdem-server
+
+go 1.18
+
+require github.com/gorilla/websocket v1.5.0 // indirect
---
+++ player_info.go
@@ -0,0 +1,29 @@
+package main
+
+import "encoding/json"
+
+func UnmarshalPlayerInfo(data []byte) (PlayerInfo, error) {
+	var r PlayerInfo
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *PlayerInfo) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+type PlayerInfo struct {
+	PlayerList []PlayerList `json:"player_list"`
+	DataType   string       `json:"data_type"`
+}
+
+type PlayerList struct {
+	ID         int64   `json:"id"`
+	Name       string  `json:"name"`
+	Balance    float64 `json:"balance"`
+	Bet        int64   `json:"bet"`
+	Status     string  `json:"status"`
+	Role       string  `json:"role"`
+	IsOperator bool    `json:"is_operator"`
+	CardInHand []int64 `json:"card_in_hand"`
+}
